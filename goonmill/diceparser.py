acc2ed17f5832c24d0dbbc533fc8ea673ef4788b
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-04-25 16:34:37 -0700

    Parser now supports filter (4d6h3) and sort (4d6h3x7sort)

--- goonmill/diceparser.py
+++ goonmill/diceparser.py
@@ -5,14 +5,22 @@ from simpleparse.common import numbers
 grammar = r'''# RPG-STYLE DICE EXPRESSIONS
 <ws> := [ \t]*
 <n> := int
+
 dieSize := 'd',ws,n
 count := n
 dieSet := count?,ws,dieSize
+
+filter := [hHlL], n
+
 dieModifier := [+-],ws,n
+
 repeat := n
-rollRepeat := 'x',ws,repeat
+sorted := 'sort'
+rollRepeat := 'x',ws,repeat,ws,sorted?
+
+randomNumber := dieSet,ws,filter?,dieModifier?
 staticNumber := n,ws,?-'d'
-randomNumber := dieSet,ws,dieModifier?
+
 diceExpression := (staticNumber/randomNumber),ws,rollRepeat?
 
 # normally my root production will not get called in the Processor,
@@ -26,12 +34,19 @@ class DiceExpression(object):
         self.dieSize = 1
         self.dieModifier = 0
         self.repeat = 1
+        self.sort = ''
+        self.filterDirection = 'h'
+        self.filterCount = None
         self.staticNumber = None
 
     def __repr__(self):
         if self.staticNumber is None:
-            return '<DiceExpression %dd%d%+dx%d>' % (self.count, self.dieSize,
-                    self.dieModifier, self.repeat)
+            filter = ''
+            if self.filterCount is not None:
+                filter = '%s%d' % (self.filterDirection, self.filterCount)
+
+            return '<DiceExpression %dd%d%s%+dx%d%s>' % (self.count, self.dieSize,
+                    filter, self.dieModifier, self.repeat, self.sort)
         return '<DiceExpression %d (static)>' % (self.staticNumber,)
 
 class Processor(disp.DispatchProcessor):
@@ -49,6 +64,13 @@ class Processor(disp.DispatchProcessor):
     def dieSet(self, (t,s1,s2,sub), buffer):
         return disp.dispatchList(self, sub, buffer)
 
+    def sorted(self, (t,s1,s2,sub), buffer):
+        self.expr.sort = 'sort'
+
+    def filter(self, (t,s1,s2,sub), buffer):
+        self.expr.filterDirection = buffer[s1].lower()
+        self.expr.filterCount = int(buffer[s1+1:s2])
+
     def count(self, (t,s1,s2,sub), buffer):
         self.expr.count = int(buffer[s1:s2])
 
@@ -62,7 +84,7 @@ class Processor(disp.DispatchProcessor):
         self.expr.repeat = int(buffer[s1:s2])
 
     def rollRepeat(self, (t,s1,s2,sub), buffer):
-        return disp.dispatch(self, sub[0], buffer)
+        return disp.dispatchList(self, sub, buffer)
 
 diceParser = parser.Parser(grammar, root="diceExpressionRoot")
 
@@ -74,16 +96,20 @@ diceExpression = objectgenerator.LibraryElement(
 		production = 'diceExpression',
         )
 
-tests = [
+tests = [ # {{{
     ' d10',
     ' 2d20',
     '2d20+1',
     '2d 20+1',
     '2 d 20- 1',
     '1d20 +1x5 2d20',
+    "2d6-2x2sort",
+    "9d6l3-10x2",
+    "9d6H3+10x2",
     '2',
     '2d',
-]
+] # }}}
+
 
 if __name__ == '__main__':
     for test in tests:
