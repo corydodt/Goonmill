883fd6bc8ad82b0a1172ef00f8d9f1f5f13f8543
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-05-20 17:23:28 -0700

    rdfs already defines :label, so use that.

--- goonmill/rdfquery.py
+++ goonmill/rdfquery.py
@@ -4,6 +4,7 @@ from goonmill.util import RESOURCE
 from goonmill import sparqly as S
 
 from rdflib.Namespace import Namespace as NS
+from rdflib import RDFS
 
 fam = NS('http://thesoftworld.com/2007/family.n3#')
 char = NS('http://thesoftworld.com/2007/characteristic.n3#')
@@ -11,7 +12,7 @@ dice = NS('http://thesoftworld.com/2007/dice.n3#')
 pcclass = NS('http://thesoftworld.com/2007/pcclass.n3#')
 
 class Sense(S.SparqItem):
-    label = S.Literal('SELECT ?l { $key c:label ?l }', default=S.Key(transform=S.iriToTitle))
+    label = S.Literal('SELECT ?l { $key rdfs:label ?l }', default=S.Key(transform=S.iriToTitle))
     description = S.Literal('SELECT ?d { $key c:description ?d }')
 
 
@@ -24,14 +25,14 @@ def filenameAsUri(fn):
 
 
 # create the root database for my triples-based SRD
-prefixes = {'': fam, 'c': char }
+prefixes = {'': fam, 'c': char, 'rdfs': RDFS.RDFSNS}
 
 db = S.TriplesDatabase(
         str(fam),
         prefixes=prefixes,
         datasets=[filenameAsUri(RESOURCE('n3data/family.n3')),
-             filenameAsUri(RESOURCE('n3data/characteristic.n3')),
-             ],
+                  filenameAsUri(RESOURCE('n3data/characteristic.n3')),
+                  ],
         )
 
 devil = Family(db=db, key=":devil")
