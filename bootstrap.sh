00eeb8cd2a3e00b56e5359f9c0c447ee32707e54
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-05-23 16:30:21 -0700

    process subs are buggy, so use <<<here-doc instead

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,14 @@ EOF
 export errorStatus=""
 
 function testPython()
+# Use: testPython "Fix this thingie" < <(command)
+#  If "command" has no output, we pass.
+# 
+#  If there is any output, the last line is considered an error message, and we
+#  print it.  Then we set the global errorStatus.
+# 
+#  "command" should not write to stderr if possible, so use 2>&1 to redirect to
+#  stdout.
 {
     message="$1"
     # the last line read is the one we want
@@ -25,17 +33,18 @@ function testPython()
 }
 
 function p()
+# Run any python code and print its output or error to stdout.
 {
     python -c "$@" 2>&1
 }
 
-testPython "Install SQLAlchemy" < <(p 'import sqlalchemy')
-testPython "Install zope.interface" < <(p 'import zope.interface')
-t='from twisted import __version__ as v; assert v>="2.5.0", "Twisted ver. is %s" % (v,)'
-testPython "Install Twisted 2.5" < <(p "$t")
-testPython "Install Divmod Nevow" < <(p 'import nevow')
-testPython "Install simpleparse" < <(p 'import simpleparse')
-testPython "Python 2.5 is required for xml.etree" < <(p 'import xml.etree')
+testPython "Install SQLAlchemy" <<<$(p 'import sqlalchemy')
+testPython "Install zope.interface" <<<$(p 'import zope.interface')
+t="from twisted import __version__ as v; assert v>='2.5.0', 'Twisted ver. is %s' % (v,)"
+testPython "Install Twisted 2.5" <<<$(p "$t")
+testPython "Install Divmod Nevow" <<<$(p 'import nevow')
+testPython "Install simpleparse" <<<$(p 'import simpleparse')
+testPython "Python 2.5 is required for xml.etree" <<<$(p 'import xml.etree')
 
 if [ "$errorStatus" == "error" ]; then
     echo "** Errors occurred.  Please fix the above errors, then re-run this script."
