310084cb323bb41d2b95a6f6455020e7163f959a
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-05-28 09:19:44 -0700

    remove all the useless classes from specialparser

--- goonmill/specialparser.py
+++ goonmill/specialparser.py
@@ -11,6 +11,7 @@ from simpleparse.common import chartypes, numbers
 
 from goonmill import query
 
+
 grammar = ( # {{{
 r'''# special quality stat
 <wsc> := [ \t]
@@ -70,102 +71,23 @@ specialQualityStat := empty/(quality, !, (sep, ws, quality)*)
 specialQualityRoot := specialQualityStat
 ''') # }}}
 
+
 specialQualityParser = parser.Parser(grammar, root='specialQualityRoot')
 
 
 class Quality(object):
     count = 0
-    def __init__(self):
+    unknowns = {}
+    def __init__(self, type, text):
         Quality.count = Quality.count + 1
-
-    def __repr__(self):
-        if self.repr:
-            return self.repr
-        return super(Quality, self).__repr__()
-
-
-class UnknownQuality(Quality):
-    frequency = {}
-    def __init__(self, s):
-        s = s.lower()
-        self.s = s
-        fq = self.frequency.get(s)
-        if fq is None:
-            self.frequency[s] = 1
-        else:
-            self.frequency[s] = fq + 1
-
-    def __repr__(self):
-        return '<UQ %s>' % (self.s,)
-
-class AlternateForm(Quality):
-    repr = "AF"
-
-class KeenSenses(Quality):
-    repr = "KS"
-
-class Telepathy(Quality):
-    repr = "T"
-
-class BlindSight(Quality):
-    repr = "BSi"
-
-class BlindSense(Quality):
-    repr = "BS"
-
-class Immunity(Quality):
-    repr = "I"
-
-class Resistance(Quality):
-    repr = "R"
-
-class FastHealing(Quality):
-    repr = "FH"
-
-class DamageReduction(Quality):
-    repr = "DR"
-
-class Regeneration(Quality):
-    repr = "Rg"
-
-class Family(Quality):
-    repr = "F"
-
-class Vulnerability(Quality):
-    repr = "V"
-
-class Darkvision(Quality):
-    repr = "DV"
-
-class Spells(Quality):
-    repr = "S"
-
-class Scent(Quality):
-    repr = "Sc"
-
-class Tremorsense(Quality):
-    repr = "Tm"
-
-class SeeInDarkness(Quality):
-    repr = "SID"
-
-class AllAroundVision(Quality):
-    repr = "AAV"
-
-class LowLightVision(Quality):
-    repr = "LLV"
-
-class WaterBreathing(Quality):
-    repr = "WB"
-
-class SpellLike(Quality):
-    repr = "SL"
-
-class Aura(Quality):
-    repr = "Au"
-
-class Empathy(Quality):
-    repr = "Em"
+        self.type = type
+        self.text = text
+        if type == 'unknown':
+            uq = Quality.unknowns.get(text)
+            if uq is None:
+                self.unknowns[text] = 1
+            else:
+                self.unknowns[text] = uq + 1
 
 
 spellLikes = { # {{{
@@ -232,6 +154,7 @@ spellLikes = { # {{{
 'displacement': 1,
 } # }}}
 
+
 class Processor(disp.DispatchProcessor):
     def specialQualityStat(self, (t,s1,s2,sub), buffer):
         self.specialQualities = []
@@ -239,78 +162,63 @@ class Processor(disp.DispatchProcessor):
         return self.specialQualities
 
     def aura(self, (t,s1,s2,sub), buffer):
-        q = Aura()
-        q.s = buffer[s1:s2]
+        q = Quality('aura', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def waterBreathing(self, (t,s1,s2,sub), buffer):
-        q = WaterBreathing()
-        q.s = buffer[s1:s2]
+        q = Quality('waterBreathing', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def alternateForm(self, (t,s1,s2,sub), buffer):
-        q = AlternateForm()
-        q.s = buffer[s1:s2]
+        q = Quality('alternateForm', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def keenSenses(self, (t,s1,s2,sub), buffer):
-        q = KeenSenses()
-        q.s = buffer[s1:s2]
+        q = Quality('keenSenses', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def tremorsense(self, (t,s1,s2,sub), buffer):
-        q = Tremorsense()
-        q.s = buffer[s1:s2]
+        q = Quality('tremorsense', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def telepathy(self, (t,s1,s2,sub), buffer):
-        q = Telepathy()
-        q.s = buffer[s1:s2]
+        q = Quality('telepathy', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def scent(self, (t,s1,s2,sub), buffer):
-        q = Scent()
-        q.s = buffer[s1:s2]
+        q = Quality('scent', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def spells(self, (t,s1,s2,sub), buffer):
-        q = Spells()
-        q.s = buffer[s1:s2]
+        q = Quality('spells', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def blindSense(self, (t,s1,s2,sub), buffer):
-        q = BlindSense()
-        q.s = buffer[s1:s2]
+        q = Quality('blindSense', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def blindSight(self, (t,s1,s2,sub), buffer):
-        q = BlindSight()
-        q.s = buffer[s1:s2]
+        q = Quality('blindSight', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def darkvision(self, (t,s1,s2,sub), buffer):
-        q = Darkvision()
-        q.s = buffer[s1:s2]
+        q = Quality('darkvision', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def allAroundVision(self, (t,s1,s2,sub), buffer):
-        q = AllAroundVision()
-        q.s = buffer[s1:s2]
+        q = Quality('allAroundVision', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def seeInDarkness(self, (t,s1,s2,sub), buffer):
-        q = SeeInDarkness()
-        q.s = buffer[s1:s2]
+        q = Quality('seeInDarkness', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def lowLightVision(self, (t,s1,s2,sub), buffer):
-        q = LowLightVision()
-        q.s = buffer[s1:s2]
+        q = Quality('lowLightVision', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def vulnerability(self, (t,s1,s2,sub), buffer):
-        q = Vulnerability()
-        q.s = buffer[s1:s2]
+        q = Quality('vulnerability', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def immunity(self, (t,s1,s2,sub), buffer):
@@ -320,43 +228,35 @@ class Processor(disp.DispatchProcessor):
             assert buf.startswith('immunity')
             imms = buf[len('immunity to'):].split(' and ')
             for imm in imms:
-                q = Immunity()
-                q.s = 'immunity to %s' % (imm.strip(),)
+                q = Quality('immunity', imm.strip())
                 newQualities.append(q)
         else:
-            q = Immunity()
-            q.s = buffer[s1:s2]
+            q = Quality('immunity', buffer[s1:s2])
             newQualities = [q]
         self.specialQualities.extend(newQualities)
 
     def resistance(self, (t,s1,s2,sub), buffer):
-        q = Resistance()
-        q.s = buffer[s1:s2]
+        q = Quality('resistance', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def regeneration(self, (t,s1,s2,sub), buffer):
-        q = Regeneration()
-        q.s = buffer[s1:s2]
+        q = Quality('regeneration', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def damageReduction(self, (t,s1,s2,sub), buffer):
-        q = DamageReduction()
-        q.s = buffer[s1:s2]
+        q = Quality('damageReduction', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def fastHealing(self, (t,s1,s2,sub), buffer):
-        q = FastHealing()
-        q.s = buffer[s1:s2]
+        q = Quality('fastHealing', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def empathy(self, (t,s1,s2,sub), buffer):
-        q = Empathy()
-        q.s = buffer[s1:s2]
+        q = Quality('empathy', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def family(self, (t,s1,s2,sub), buffer):
-        q = Family()
-        q.s = buffer[s1:s2]
+        q = Quality('family', buffer[s1:s2])
         self.specialQualities.append(q)
 
     def empty(self, (t,s1,s2,sub), buffer):
@@ -365,11 +265,10 @@ class Processor(disp.DispatchProcessor):
     def unknownQuality(self, (t,s1,s2,sub), buffer):
         s = buffer[s1:s2]
         if s.lower() in spellLikes:
-            q = SpellLike()
-            q.s = s
+            q = Quality('spellLike', s)
             self.specialQualities.append(q)
         else:
-            q = UnknownQuality(buffer[s1:s2])
+            q = Quality('unknown', buffer[s1:s2])
             self.specialQualities.append(q)
 
 
@@ -381,7 +280,7 @@ def parseSpecialQualities(s):
 
 
 def printFrequenciesOfUnknowns():
-    items = UnknownQuality.frequency.items()
+    items = Quality.unknowns.items()
     for n, (k, freq) in enumerate(items):
         items[n] = freq, k
     import pprint
