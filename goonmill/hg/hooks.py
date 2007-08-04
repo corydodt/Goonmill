8af0bb8bb58c6399e36c91ce74d68ac841a66f17
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-08-04 13:44:45 -0400

    debugging hooks

--- goonmill/hg/hooks.py
+++ goonmill/hg/hooks.py
@@ -7,6 +7,7 @@ from mercurial.node import bin
 import os
 
 def changegroupRestart(ui, repo, **kw):
+    import pdb; pdb.set_trace()
     node = bin(kw['node'])
     os.system("make goonmill-stop")
     os.system("make goonmill-start")
