45be47595265d12e9455da9b7314b37196ba51e6
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-08-04 14:03:40 -0400

    docstring

--- goonmill/hg/hooks.py
+++ goonmill/hg/hooks.py
@@ -7,6 +7,7 @@ from mercurial.node import bin
 import os
 
 def commitRestart(ui, repo, **kw):
+    """Restart the goonmill server on every commit"""
     node = bin(kw['node'])
     os.chdir(repo.path + '/..')
     os.system("make goonmill-stop")
