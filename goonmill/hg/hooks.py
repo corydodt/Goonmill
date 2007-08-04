866a410f7ed88b1b0092733b7927a15f227cde56
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-08-04 13:56:43 -0400

    debugging hooks - add a separate changegroup hook

--- goonmill/hg/hooks.py
+++ goonmill/hg/hooks.py
@@ -6,11 +6,12 @@ from mercurial.node import bin
 
 import os
 
-def changegroupRestart(ui, repo, **kw):
+def commitRestart(ui, repo, **kw):
     node = bin(kw['node'])
     os.chdir(repo.path + '/..')
     os.system("make goonmill-stop")
     os.system("make goonmill-start")
 
-# commit hook uses the same code as changegroup hook
-commitRestart = changegroupRestart
+def changegroupRestart(ui, repo, **kw):
+    node = bin(kw['node'])
+    import pdb; pdb.set_trace()
