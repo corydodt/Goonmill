469eeade871f359eb9807d2c5635826408e26be5
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-08-04 13:54:14 -0400

    debugging hooks

--- goonmill/hg/__init__.py
+++ goonmill/hg/__init__.py
@@ -1,1 +1,13 @@
-"""Mercurial stuff"""
+"""Mercurial stuff
+
+To use, add
+
+[extensions]
+goonmillext = /.../goonmill/hg/hooks.py
+
+[hooks]
+changegroup = hg up
+changegroup = goonmillext.changegroupRestart
+commit = goonmillext.commitRestart
+
+"""
