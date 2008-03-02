8e7e06fbc3f59f74a862b658ba1cd214b175e827
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2008-03-01 22:52:18 -0800

    update search code to recent pylucene

--- goonmill/search.py
+++ goonmill/search.py
@@ -1,19 +1,17 @@
-"""PyLucene-based indexing and searching
-"""
+"""PyLucene-based indexing and searching """
 # vi:ft=python
 import sys, os
-
+import shutil
 import re
 
-from lucene import (IndexWriter, StandardAnalyzer, Document, Field,
-        MultiFieldQueryParser, IndexSearcher, initVM, CLASSPATH, Hit,
-        FSDirectory)
-initVM(CLASSPATH)
+import lucene
+lucene.initVM(lucene.CLASSPATH)
 
 from twisted.web import microdom, domhelpers
 from twisted.python import usage
 
 from goonmill.util import RESOURCE
+INDEX_DIRECTORY = RESOURCE('lucene-data')
 
 slashRx = re.compile(r'\\([n"])')
 
@@ -26,14 +24,14 @@ def indexMonster(writer, monster):
     _ft = re.sub(slashRx, repSlash, monster.full_text)
     full = textFromHtml(_ft)
 
-    doc = Document()
-    name_ = Field("name_", monster.name,
-                  Field.Store.YES, Field.Index.TOKENIZED)
+    doc = lucene.Document()
+    name_ = lucene.Field("name_", monster.name,
+                  lucene.Field.Store.YES, lucene.Field.Index.TOKENIZED)
     name_.setBoost(2.0)
-    full_text = Field("full_text", full,
-                  Field.Store.YES, Field.Index.TOKENIZED)
-    id = Field("id", str(monster.id),
-                  Field.Store.YES, Field.Index.UN_TOKENIZED)
+    full_text = lucene.Field("full_text", full,
+                  lucene.Field.Store.YES, lucene.Field.Index.TOKENIZED)
+    id = lucene.Field("id", str(monster.id),
+                  lucene.Field.Store.YES, lucene.Field.Index.UN_TOKENIZED)
     doc.add(name_)
     doc.add(full_text)
     doc.add(id)
@@ -77,18 +75,25 @@ class MyHit(object):
 
 def find(terms):
     """Use the Lucene index to find monsters"""
-    dataDir = FSDirectory.getDirectory(RESOURCE('lucene-data'), False)
-    searcher = IndexSearcher(dataDir)
-    qp = MultiFieldQueryParser(['name', 'full_text'], StandardAnalyzer())
-    qp.setDefaultOperator(MultiFieldQueryParser.Operator.AND)
     fuzzy = [fuzzyQuoteTerm(t) for t in terms]
     terms = ' '.join(fuzzy)
-    query = qp.parse(qp, terms) # wtf, qp.parse(qp...) ??
+    dataDir = lucene.FSDirectory.getDirectory(INDEX_DIRECTORY, False)
+    searcher = lucene.IndexSearcher(dataDir)
+
+    MFQP = lucene.MultiFieldQueryParser
+
+    SHOULD = lucene.BooleanClause.Occur.SHOULD
+
+    query = MFQP.parse(terms, 
+            ['name_', 'full_text'], 
+            [SHOULD, SHOULD], 
+            lucene.StandardAnalyzer())
+
     hits = searcher.search(query)
 
     ret = []
     for i, hit in enumerate(hits):
-        doc = Hit.cast_(hit).getDocument()
+        doc = lucene.Hit.cast_(hit).getDocument()
         ret.append(MyHit(doc, hits, i))
         if i == 10:
             break
@@ -97,22 +102,42 @@ def find(terms):
 
 
 def buildIndex(monsters):
-    _dataDir = RESOURCE('lucene-data')
-    if os.path.exists(_dataDir):
+    if os.path.exists(INDEX_DIRECTORY):
         return
-    dataDir = FSDirectory.getDirectory(_dataDir, False)
-    writer = IndexWriter(dataDir, StandardAnalyzer(), True)
+    dataDir = lucene.FSDirectory.getDirectory(INDEX_DIRECTORY, True)
+    writer = lucene.IndexWriter(dataDir, lucene.StandardAnalyzer(), True)
     for monster in monsters:
         indexMonster(writer, monster)
+    writer.optimize()
+    writer.close()
 
 
 class Options(usage.Options):
+    optFlags = [
+            ['build-index', 'b', 'Build a fresh index']
+
+            ]
+
     def parseArgs(self, *terms):
         self['terms'] = terms
 
     def postOptions(self):
-        for hit in find(self['terms']):
-            print hit.name, hit.score
+        if self['build-index']:
+            try:
+                shutil.rmtree(INDEX_DIRECTORY)
+            except EnvironmentError, e:
+                pass
+
+            from goonmill.query2 import db
+            buildIndex(db.allMonsters())
+
+        try:
+            for hit in find(self['terms']):
+                print hit.name, hit.score
+        except lucene.JavaError, e:
+            if 'FileNotFoundException' in str(e):
+                raise usage.UsageError(
+                        "** Missing index directory.  Run with --build-index")
 
 
 
