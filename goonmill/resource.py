5d848d3c458c95f8a222eb7d3eec988423705ac8
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2008-03-30 12:31:32 -0700

    add guard stuff and look up workspaces from the database

--- goonmill/resource.py
+++ goonmill/resource.py
@@ -3,9 +3,16 @@ The resource structure for Goonmill
 
 API for building the goonmill page and its fragments.
 """
-from nevow import rend, url, loaders, athena, static
+import random
 
-from goonmill.util import RESOURCE
+from nevow import rend, url, loaders, athena, static, guard
+
+from twisted.cred.portal import Portal
+from twisted.cred.credentials import IAnonymous
+from twisted.cred.checkers import AllowAnonymousAccess
+
+from .util import RESOURCE
+from .user import Workspace
 
 class Root(rend.Page):
     """
@@ -13,21 +20,90 @@ class Root(rend.Page):
     resources.
     """
     addSlash = True  # yeah, we really do need this, otherwise 404 on /
+    realm = None
+    checkers = None
+    _wrapper = None
 
     def child_static(self, ctx):
         return static.File(RESOURCE('static'))
 
     def child_app(self, ctx):
-        return Workspace()
+        if self._wrapper is None:
+            self._wrapper = guardedWrapper(self.realm, self.checkers)
+        return self._wrapper
 
     def renderHTTP(self, ctx):
         return url.root.child("app")
 
 
-class Workspace(athena.LivePage):
+class GuardedRoot(rend.Page):
+    """
+    The /app part of webspace
+    """
+    addSlash = True
+
+    def __init__(self, user, userDatabase, *a, **kw):
+        self.user = user
+        self.userDatabase = userDatabase
+        rend.Page.__init__(self, *a, **kw)
+
+    def renderHTTP(self, ctx):
+        """Redirect to a new empty workspace"""
+        x = random.random()
+        u = url.URL.fromString('/app/ws/%s' % (x,))
+        return u
+
+    def locateChild(self, ctx, segs):
+        if segs[0] == 'ws':
+            # look up workspace. if it belongs to a user, make sure it belongs
+            # to me.
+            key = unicode(segs[1])
+            ws = self.userDatabase.store.find(Workspace, Workspace.url ==
+                    key).one()
+
+            if self.user is None:
+                if not ws:
+                    ws = Workspace()
+                    ws.url = key
+                    self.userDatabase.store.add(ws)
+                    self.userDatabase.store.commit()
+                page = WorkspacePage(ws, None)
+                return (page, segs[2:])
+
+            else:
+                if ws in self.user.workspaces:
+                    page = WorkspacePage(ws, self.user)
+                    return (page, segs[2:])
+
+            return (None, ())
+
+        return rend.Page.locateChild(self, ctx, segs)
+
+
+def guardedWrapper(realm, checkers):
+    ptl = Portal(realm)
+    ptl.registerChecker(AllowAnonymousAccess(), IAnonymous)
+    for checker in checkers:
+        ptl.registerChecker(checker)
+    # FIXME - this should be another wrapper, which returns a SessionWrapper,
+    # which has persistentCookies set or not.  for now, all cookies are
+    # persistent for a week
+    wrap = guard.SessionWrapper(ptl)
+    wrap.persistentCookies = True
+    wrap.sessionLifetime = 3600 * 24 * 7
+    return wrap
+
+
+class WorkspacePage(athena.LivePage):
     docFactory = loaders.xmlfile(RESOURCE('templates/workspace.xhtml'))
     addSlash = 1
 
+    def __init__(self, workspace, user, *a, **kw):
+        self.workspace = workspace
+        self.user = user
+        athena.LivePage.__init__(self, *a, **kw)
+
     def render_workspace(self, ctx, data):
         return ctx.tag
+
 
--- twisted/plugins/goonmilltap.py
+++ twisted/plugins/goonmilltap.py
@@ -6,50 +6,14 @@ Twisted 2.5 or later is required to use this.
 
 from zope.interface import implements
 
-from twisted.python import usage, log
+from twisted.python import usage
 from twisted.plugin import IPlugin
 from twisted.application.service import IServiceMaker
-from twisted.application import internet
-
-from nevow import appserver
-from goonmill.resource import Root
-from goonmill.query2 import db
 
 class Options(usage.Options):
     optParameters = [['port', 'p', '6680', 'Port to run on'],
                      ]
     optFlags = [['dev', None, 'Enable development features such as /sandbox']]
-
-
-class STFUSite(appserver.NevowSite):
-    """Website with <80 column logging"""
-    def log(self, request):
-        uri = request.uri
-
-        if 'jsmodule' in uri:
-            uris = uri.split('/')
-            n = uris.index('jsmodule')
-            uris[n-1] = uris[n-1][:3] + '...'
-            uri = '/'.join(uris)
-
-        if len(uri) > 38:
-            uri = '...' + uri[-35:]
-
-        code = request.code
-        if code != 200:
-            code = '!%s!' % (code, )
-
-        log.msg('%s %s' % (code, uri), system='HTTP', )
-
-
-class GoonmillService(internet.TCPServer):
-    def startService(self, *a, **kw):
-        # build the search index.  block on this, it has to happen before app
-        # startup.
-        from goonmill import search
-        search.buildIndex(db.allMonsters())
-
-        internet.TCPServer.startService(self, *a, **kw)
 
 
 class GoonmillServerMaker(object):
@@ -67,11 +31,13 @@ class GoonmillServerMaker(object):
 
     def makeService(self, options):
         """
-        Construct the test daemon.
+        Construct the goonmill
         """
-        resource = Root()
-        factory = STFUSite(resource)
-        return GoonmillService(int(options['port']), factory)
+        from goonmill.webserver import WebServer, WebSite
+        site = WebSite()
+        ws = WebServer(int(options['port']), site)
+        ws.site = site
+        return ws
 
 # Now construct an object which *provides* the relevant interfaces
 
