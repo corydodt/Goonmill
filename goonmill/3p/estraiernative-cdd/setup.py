33b68e8412a46698b092b7d24e0995137e0be544
Cory Dodt <corymercurial@spam.goonmill.org>
2008-11-30 23:53:28 -0800

    create a suitable setup.py for pypi

--- goonmill/3p/estraiernative-cdd/setup.py
+++ goonmill/3p/estraiernative-cdd/setup.py
@@ -1,11 +1,34 @@
 #
-# $Id: setup.py,v 1.2 2007/04/15 05:52:47 yosida Exp $
-#
-
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 ext = Extension("_estraiernative",
                 ["estraiernative.c"],
                 libraries=["estraier"],
-                include_dirs=["/usr/include/estraier", "/usr/include/qdbm"])
-setup(name="estraiernative", version="0.2", ext_modules=[ext])
+                include_dirs=["/usr/include/estraier", "/usr/include/qdbm"],
+                )
+
+setup(
+        name="Hypy", 
+        description='Pythonic wrapper for Hyper Estraier',
+        author='Yusuke YOSHIDA',
+        author_email='usk@nrgate.jp',
+        maintainer='Cory Dodt',
+        maintainer_email='pypi@spam.goonmill.org',
+        url='http://goonmill.org/hypy/',
+        download_url='http://hypy-source.goonmill.org/archive/tip.tar.gz',
+        version="0.2.1", 
+        ext_modules=[ext],
+        py_modules=['hypy'],
+
+        classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+          ],
+      )
