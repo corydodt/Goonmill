040a96c72c10bc99c78df5bde0dae3caaf2299ec
Cory Dodt <corymercurial@spam.goonmill.org>
2008-12-08 23:10:53 -0800

    interim - mostly work on making it unit testable

--- goonmill/3p/hypy/hypy.py
+++ goonmill/3p/hypy/hypy.py
@@ -3,7 +3,7 @@ Put a Pythonic face on estraiernative
 """
 
 from _estraiernative import (Condition as CCondition,
-    Database as CDatabase, Document as CDocument)
+    Database as CDatabase, Document as CDocument, EstError as CError)
 
 class PutFailed(Exception):
     """
@@ -37,16 +37,16 @@ class CloseFailed(Exception):
         return self.message
 
 
-class ECondition(object):
+class HCondition(object):
     """
     A search condition.
     Use matching='simple', 'rough', 'union' or 'isect'
     """
     def __init__(self, phrase, matching='simple', max=None):
         self.condition = CCondition()
-        self.set_phrase(phrase)
+        self.condition.set_phrase(phrase)
         if max is not None:
-            self.set_max(max)
+            self.condition.set_max(max)
         flags = 0
         if matching == 'simple':
             flags |= CCondition.SIMPLE
@@ -56,21 +56,20 @@ class ECondition(object):
             flags |= CCondition.UNION
         elif matching == 'isect':
             flags |= CCondition.ISECT
-        self.set_options(flags)
-
-    def __getattr__(self, name):
-        return getattr(self.condition, name)
+        self.condition.set_options(flags)
 
 
-class EDatabase(object):
+class HDatabase(object):
     """
     A more pythonic interface to estraier's database
     """
+    # TODO - pass an optional encoding into __init__ (default utf-8) to
+    # specify the desired encoding.  after a database is created, write a
+    # special file in it that remembers the encoding.  read that file on
+    # opening to set an instance variable.  replace all 'utf-8' with
+    # self.encoding in database accesses
     def __init__(self):
-        self.database = CDatabase()
-
-    def __getattr__(self, name):
-        return getattr(self.database, name)
+        self._cdb = CDatabase()
 
     def putDoc(self, doc, clean=False, weight=False):
         """
@@ -78,12 +77,12 @@ class EDatabase(object):
         """
         flags = 0
         if clean:
-            flags = flags | self.PDCLEAN
+            flags = flags | self._cdb.PDCLEAN
         if weight:
-            flags = flags | self.PDWEIGHT
-        if not self.put_doc(doc, flags):
-            msg = self.err_msg(self.error())
-            raise EstraierPutFailed(doc.attr('@uri'), msg)
+            flags = flags | self._cdb.PDWEIGHT
+        if not self._cdb.put_doc(doc._cdoc, flags):
+            msg = self._cdb.err_msg(self._cdb.error())
+            raise PutFailed(doc[u'@uri'], msg)
 
         return doc
 
@@ -97,16 +96,16 @@ class EDatabase(object):
         """
         flags = 0
         if mode == 'a':
-            flags = self.DBWRITER | self.DBCREAT
+            flags = self._cdb.DBWRITER | self._cdb.DBCREAT
         elif mode == 'w':
-            flags = self.DBWRITER | self.DBCREAT | self.DBTRUNC
+            flags = self._cdb.DBWRITER | self._cdb.DBCREAT | self._cdb.DBTRUNC
         elif mode == 'r':
-            flags = self.DBREADER
+            flags = self._cdb.DBREADER
 
         assert flags, "mode must be 'a', 'w' or 'r'"
 
-        if not self.database.open(directory, flags):
-            msg = self.err_msg(self.error())
+        if not self._cdb.open(directory, flags):
+            msg = self._cdb.err_msg(self._cdb.error())
             raise OpenFailed(msg)
 
         return self
@@ -115,61 +114,151 @@ class EDatabase(object):
         """
         Put the database down for the night.
         """
-        if not self.database.close():
-            msg = self.err_msg(self.error())
+        failed = False
+        try:
+            if not self._cdb.close():
+                failed = True
+        except CError:
+            failed = True
+        if failed:
+            msg = self._cdb.err_msg(self._cdb.error())
             raise CloseFailed(msg)
 
     def search(self, condition):
         """
         Submit a query to the database and return the results object.
         """
-        result = self.database.search(condition.condition)
-        return EResults(self, result)
+        result = self._cdb.search(condition.condition)
+        return HResults(self, result)
+
+    def walkResult(self, result):
+        count = result.doc_num()
+        for i in range(count):
+            doc = self._cdb.get_doc(result.get_doc_id(i), 0) # TODO - flags
+
+            if doc is None: # XXX WTF? is this for a race condition against
+                continue    # someone removing docs from the index?
+
+            yield HHit.fromCDocument(doc)
 
 
-class EResults(list):
+class HResults(list):
     """
     List wrapper for results of the search.
     """
     def __init__(self, db, result):
-        l = []
-        count = result.doc_num()
-        for i in range(count):
-            doc = db.get_doc(result.get_doc_id(i), 0) # TODO - flags
+        self._cresult = result
+        list.__init__(self, db.walkResult(result))
 
-            if doc is None: # XXX WTF? is this for a race condition against
-                            # someone removing docs from the index?
-                continue
+    def hintWords(self):
+        """
+        Return the unicode-d search terms
+        """
+        return [w.decode('utf-8') for w in self._cresult.hint_words()]
 
-            l.append(EHit(doc, result.hint_words()))
-        list.__init__(self, l)
+    def pluck(self, attribute):
+        """
+        Return the value of the given attribute for each result, in a list
+        """
+        return [h[attribute] for h in self]
 
 
-class EHit(object):
+class HDocument(object):
     """
-    Dict-like interface to a document returned from a search
+    Dict-like interface to a document
     """
-    def __getattr__(self, name):
-        return getattr(self.hit, name)
+    def __str__(self):
+        return self._cdoc.dump_draft()
 
-    def __init__(self, hit, terms):
-        self.hit = hit
-        self.terms = terms
+    def __init__(self, uri):
+        if not type(uri) is type(u''):
+            raise TypeError("Must provide uri as unicode text")
+        self._cdoc = CDocument()
+        self._cdoc.add_attr('@uri', uri.encode('utf-8'))
+
+    def addHiddenText(self, text):
+        if not type(text) is type(u''):
+            raise TypeError("Must provide unicode text")
+        t = text.encode('utf-8')
+        self._cdoc.add_hidden_text(t)
+
+    def addText(self, text):
+        if not type(text) is type(u''):
+            raise TypeError("Must provide unicode text")
+        t = text.encode('utf-8')
+        self._cdoc.add_hidden_text(t)
+
+    @classmethod
+    def fromCDocument(cls, cdocument):
+        self = cls(uri=cdocument.attr('@uri').decode('utf8'))
+        self._cdoc = cdocument
+        return self
+
+    def __delitem__(self, name):
+        raise NotImplemented("Cannot delete attributes from this document")
+
+    def __setitem__(self, name, value):
+        if not type(name) is type(value) is type(u''):
+            raise TypeError("Must provide unicode key and value")
+        v = value.encode('utf-8')
+        self._cdoc.add_attr(name, v)
 
     def __getitem__(self, name):
-        return self.attr(name)
+        name = name.decode('utf-8')
+        if name not in self._cdoc.attr_names():
+            raise KeyError(name)
+        return self._cdoc.attr(name).decode('utf-8')
 
+    def update(self, other):
+        for k, v in other.items():
+            self[k] = v
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def keys(self):
+        return [n.decode('utf-8') for n in self._cdoc.attr_names()]
+
+    def values(self):
+        return [self[a] for a in self.keys()]
+
+    def items(self):
+        return [(a,self[a]) for a in self.keys()]
+
+    def getTexts(self):
+        TODO # return a dict of each text, and each hidden?
+
+    def _get_id(self):
+        return self._cdoc.id()
+    id = property(_get_id)
+
+
+class HHit(HDocument):
+    """
+    Dict-like interface to a document
+    """
     def teaser(self, terms, format='html'):
         """
         Produce the teaser/snippet text for the hit
         """
         if format != 'html':
             raise NotImplementedError("Only html format supported for teaser text")
-        # arguments to make_snippet MUST be byte strings
-        terms = [t.encode('utf8') for t in terms]
-        snip = self.make_snippet(terms, 120, 35, 35)
-        bunches = snip.split('\n\n')
 
+        # arguments to make_snippet MUST be byte strings
+        bterms = []
+        for t in terms:
+            if not type(t) is type(u''):
+                raise TypeError("Terms must be unicode text")
+            bterms.append(t.encode('utf8'))
+
+        # TODO - parameterize make_snippet's numbers
+        snip = self._cdoc.make_snippet(bterms, 120, 35, 35)
+
+        # parse the newline-delimited snippet format
+        bunches = snip.split('\n\n')
         strings = []
         for bunch in snip.split('\n\n'):
             _bitStrings = []
