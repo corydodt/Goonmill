77ab9fd5d1e3557f411bc504a0353ce0cfb68f4a
Cory Dodt <corymercurial@spam.goonmill.org>
2008-12-08 23:14:32 -0800

    put hypy inside a package

---
+++ goonmill/3p/hypy/hypy/__init__.py
@@ -0,0 +1,10 @@
+"""Hypy - Pythonic Hyper Estraier"""
+from .lib import (PutFailed,
+    OpenFailed,
+    CloseFailed,
+    HCondition,
+    HDatabase,
+    HResults,
+    HDocument,
+    HHit,
+    )
