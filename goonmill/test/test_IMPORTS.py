45f391e1be540cd93636ab94cae7882847c9b7d7
Cory Dodt <corymercurial@spam.goonmill.org>
2009-05-10 13:23:38 -0700

    purge a bunch of stuff that moved into playtools

--- goonmill/test/test_IMPORTS.py
+++ goonmill/test/test_IMPORTS.py
@@ -9,8 +9,6 @@ class IMPORTTestCase(unittest.TestCase):
     Just import things and make sure modules weren't deleted
     """
     def test_importStuff(self):
-        from ..query import db, allSpells, allMonsters
-        from ..search import find
         from ..util import RESOURCE
         from ..userfile import StaticImage, userFolder
         from ..fileupload import FileUploadPage
