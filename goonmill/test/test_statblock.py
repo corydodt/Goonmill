d2a52f64a84928db301c99d27c6ef0a824e0f44b
Cory Dodt <corymercurial@spam.goonmill.org>
2009-05-13 23:30:53 -0700

    improve the organization and coverage of tests in statblock

--- goonmill/test/test_statblock.py
+++ goonmill/test/test_statblock.py
@@ -2,31 +2,53 @@
 Use the statblock module to format monsters
 """
 
-import unittest
+from twisted.trial import unittest
 from .. import statblock
 from playtools import fact
+from playtools.test.util import pluck
 
 SRD = fact.systems['D20 SRD']
 MONSTERS = SRD.facts['monster']
 
 class StatblockTestCase(unittest.TestCase):
-    def test_statblock(self):
+    def test_statblockQuick(self):
+        """
+        Basic properties of the monster can be read
+        """
         sb = statblock.Statblock.fromId(379)
         self.assertEqual(sb.get('alignment'), 'Chaotic Evil') 
         self.assertEqual(sb.get('hitDice'), '2d12')
 
-        # just load all the monsters
-        monsters = MONSTERS.dump()
-        for monster in monsters:
-            exp = [monster.name, None]
-            monster = statblock.Statblock.fromId(monster.id)
-            act = [monster.get('name'), monster and None]
-            self.assertEqual(exp, act)
+    def test_statblockFromMonster(self):
+        """
+        We can construct a statblock from an instance of Monster
+        """
+        monster = MONSTERS[379]
+        sb2 = statblock.Statblock.fromMonster(monster)
+        self.assertEqual(sb2.get('alignment'), 'Chaotic Evil') 
+        self.assertEqual(sb2.get('hitDice'), '2d12')
 
     def test_parseFeats(self):
+        """
+        A monster's feats can be read and parsed into a list, and reformatted
+        back out
+        """
         dbmohrg = MONSTERS[u'Mohrg']
         mohrg = statblock.Statblock.fromMonster(dbmohrg)
         self.assertEqual(mohrg.get('acFeats'), 'Dodge, Mobility')
+        self.assertEqual(mohrg.get('feats'), 
+            u'Alertness, Dodge, Improved Initiative, Lightning Reflexes, Mobility')
+
+    def test_parseSkills(self):
+        """
+        A monster's skills can be read and parsed into a list, and reformatted
+        back out
+        """
+        dbmohrg = MONSTERS[u'Mohrg']
+        mohrg = statblock.Statblock.fromMonster(dbmohrg)
+        self.assertEqual(mohrg.get('skills'), 
+                u'Climb +13, Hide +21, Listen +11, Move Silently +21, Spot +15, Swim +9'
+                )
 
     def test_oneLine(self):
         """
@@ -37,7 +59,31 @@ class StatblockTestCase(unittest.TestCase):
                 u'<<Mohrg>> Chaotic Evil Medium Undead || Init +9 || Darkvision 60 ft., Darkvision 60 ft. Listen +11 Spot +15 || AC 23 (+4 Dex, +9 natural), touch 14, flat-footed 14 || 14d12 HD || Fort +4 Ref +10 Will +9 || 30 ft. (6 squares) || MELEE Slam +12 (1d6+7) melee, and tongue +12 (paralysis) melee || Atk Options Improved grab, paralyzing touch, create spawn || Str 21, Dex 19, Con -, Int 11, Wis 10, Cha 10 || SQ Darkvision 60 ft., undead traits || http://www.d20srd.org/srd/monsters/mohrg.htm'
                 )
 
-    def test_statblockSkill(self):
+    def test_family(self):
+        """
+        Monsters' families are correctly extracted
+        """
+        barghest = statblock.Statblock.fromMonster(MONSTERS[144])
+        actual = sorted(pluck(barghest.families, 'label'))
+        expected = sorted([u'Outsider', u'Evil', u'Extraplanar', u'Lawful',
+            u'Shapechanger'])
+        self.assertEqual(actual, expected)
+
+
+class HugeStatblockTestCase(unittest.TestCase):
+    """
+    Crude tests that run some of the code on all the monsters
+    """
+    def test_statblockALL(self):
+        # just load all the monsters
+        monsters = MONSTERS.dump()
+        for monster in monsters:
+            exp = [monster.name, None]
+            monster = statblock.Statblock.fromId(monster.id)
+            act = [monster.get('name'), monster and None]
+            self.assertEqual(exp, act)
+
+    def test_statblockSkillALL(self):
         skillStats = [m.skills for m in MONSTERS.dump()]
         for s in skillStats:
             if s is None:
@@ -50,7 +96,7 @@ class StatblockTestCase(unittest.TestCase):
             self.assertEqual(exp, act)
         #
 
-    def test_statblockFeat(self):
+    def test_statblockFeatALL(self):
         featStats = [m.feats for m in MONSTERS.dump()]
         for f in featStats:
             if f is None:
@@ -62,7 +108,7 @@ class StatblockTestCase(unittest.TestCase):
             act = [f, statblock.parseFeats(f) and []]
             self.assertEqual(exp, act)
 
-    def test_hitPoints(self):
+    def test_hitPointsALL(self):
         hpStats = [m.hit_dice for m in MONSTERS.dump()]
         for hp in hpStats:
             if hp is None:
@@ -74,7 +120,7 @@ class StatblockTestCase(unittest.TestCase):
             act = [hp, statblock.parseHitPoints(hp) and None]
             self.assertEqual(exp, act)
 
-    def test_saves(self):
+    def test_savesALL(self):
         saveStats = [m.saves for m in MONSTERS.dump()]
         for saves in saveStats:
             if saves is None:
