ec6684ae13b81ede0e35521177a3a91a1c02331e
Cory Dodt <corymercurial@spam.goonmill.org>
2009-05-25 10:30:32 -0700

    refine test_IMPORTS a bit

--- goonmill/test/test_IMPORTS.py
+++ goonmill/test/test_IMPORTS.py
@@ -8,14 +8,30 @@ class IMPORTTestCase(unittest.TestCase):
     """
     Just import things and make sure modules weren't deleted
     """
-    def test_importStuff(self):
+    def test_importUtil(self):
         from ..util import RESOURCE
         RESOURCE
+
+    def test_importUserfile(self):
         from ..userfile import StaticImage, userFolder
         StaticImage, userFolder
+
+    def test_importFileupload(self):
         from ..fileupload import FileUploadPage
         FileUploadPage
+
+    def test_importStatblock(self):
         from ..statblock import Statblock
         Statblock
+
+    def test_importResource(self):
         from ..resource import Root
         Root
+
+    def test_importAuth(self):
+        from ..auth import CookieRealm
+        CookieRealm
+
+    def test_importWebserver(self):
+        from ..webserver import WebServer
+        WebServer
