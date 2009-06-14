5fe683eb285d9c0be448bd90cf323b3095cef2b5
Cory Dodt <corymercurial@spam.goonmill.org>
2009-06-14 11:30:39 -0700

    real unit tests for saveparser

--- goonmill/parser/saveparser.py
+++ goonmill/parser/saveparser.py
@@ -121,22 +121,3 @@ class Processor(disp.DispatchProcessor):
     def qualifier(self, (t,s1,s2,sub), buffer):
         self.currentSave.qualifier = disp.getString((t,s1,s2,sub), buffer)
 
-tests = ( # {{{
-"""Fort +9, Ref +6, Will +8
-Fort -9, Ref -, Will +8
-Fort +9, Ref - , Will +8
-Fort +9, Ref +1, Will -8
-Fort +14 (+18 against poison), Ref +12, Will +12
-Fort +9*, Ref +1*, Will -8*
-As master's saves""".splitlines()) # }}}
-
-
-if __name__ == '__main__': # {{{
-    for slist in tests:
-        print slist
-        try:
-            parsed = parseSaves(slist)
-        except Exception, e:
-            raise##import sys, pdb; pdb.post_mortem(sys.exc_info()[2])
-        print parsed
-# }}}
